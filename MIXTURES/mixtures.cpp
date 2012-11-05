d10fa2f2d4ddf2c822582c2e1de054ae0f249e28
Geronimo <geronimodeabreu@gmail.com>
2012-11-04 21:56:24 -0430

    MIXTURES problem accepted C++

--- MIXTURES/mixtures.cpp
+++ MIXTURES/mixtures.cpp
@@ -25,27 +25,41 @@
 
 using namespace std;
 
+int colors(int i, int j, vector<int> mixtures){
+  int result = mixtures[i];
+  FOR(x, i+1, j+1){
+    result = (result + mixtures[x]) % 100;
+  }
+  return result;
+}
+
 int main(){
-	vector<int> test_case(4);
-	test_case.push_back(50);
-	test_case.push_back(60);
-	test_case.push_back(70);
-	test_case.push_back(90);
+  int n;
+  while(cin >> n){
+    vector<int> mixtures(n);
 
-	for(int i=1; i<test_case.size()-1; i++){
-		mix(min_mixture(0, i), min_mixture(i+1, test_case.size()));
-	}
+    FOR(i,0,n){
+      int tmp;
+      cin >> tmp;
+      mixtures[i] = tmp;
+    }
+
+    vector< vector<int> > costs(n, vector<int>(n, 100000000));
+    FOR(i, 0, n){ costs[i][i] = 0; }
+    vector<int> color(n, -1);
+
+    FOR(l, 2, n + 1){
+      FOR(i, 0, n - l + 1){
+        int j = i + l - 1;
+        FOR(k, i, j){
+          int tmp = costs[i][k] + costs[k+1][j] + (colors(i, k, mixtures) * colors(k+1, j, mixtures));
+          if(tmp < costs[i][j]){
+            costs[i][j] = tmp;
+          }
+        }
+      }
+    }
+
+    cout << costs[0][n - 1] << endl;
+  }
 }
-
-int min_mixture(i, j){
-	long long mimin = 100000000000;
-
-	if(i - j = 1){
-		return test_case[i] * test_case[j] % 100;
-	}
-	else if(i == j){
-		return test_case[i];
-
-}
-
-
--- MIXTURES/mixtures.rb
+++ MIXTURES/mixtures.rb
@@ -1,58 +1,36 @@
-class Mixture
-  attr_accessor :value
-
-  def initialize(value)
-    self.value = value
-  end
-end
-
-class Multiplicator
-  attr_accessor :chain, :costs, :colors
-
-  def initialize(chain)
-    self.chain = chain
-
-    self.costs = []
-    (0...chain.size).each do |i|
-      self.costs[i] = []
-      self.costs[i][i] = 0
-    end
-  end
-
-  def optimal_multiplication
-    n = chain.size
-    (2..n).each do |l|
-      (0..n - l).each do |i|
-        j = i + l - 1
-        self.costs[i][j] = 1 << 32
-        (i...j).each do |k|
-          tmp = self.costs[i][k] + self.costs[k+1][j] + (color(i, k) * color(k+1, j))
-          if tmp < self.costs[i][j]
-            self.costs[i][j] = tmp
-          end
+def optimal_multiplication chain
+  n = chain.size
+  (2..n).each do |l|
+    (0..n - l).each do |i|
+      j = i + l - 1
+      @costs[i][j] = 1 << 32
+      (i...j).each do |k|
+        tmp = @costs[i][k] + @costs[k+1][j] + (color(i, k, chain) * color(k+1, j, chain))
+        if tmp < @costs[i][j]
+          @costs[i][j] = tmp
         end
       end
     end
-    return self.costs[0][chain.size - 1]
   end
-  
-  def color i, j
-    result = 0
-    (i..j).each do |k|
-      result += chain[k].value
-    end
-    return result % 100
+  return @costs[0][chain.size - 1]
+end
+
+def color i, j, chain
+  return @colors[i][j] if @colors[i][j]
+  result = 0
+  (i..j).each do |k|
+    result += chain[k]
   end
+  return result % 100
 end
 
 while mixtures_count = gets do
   mixtures = gets
-  mixtures = mixtures.split(" ")
-  chain = []
-  for m in mixtures
-    chain.push Mixture.new(m.to_i)
-  end
+  mixtures = mixtures.split(" ").map { |m| m.to_i }
 
-  m = Multiplicator.new(chain)
-  puts m.optimal_multiplication
+  n = mixtures.size
+  @costs = Array.new(n) { Array.new(n) { 0 } }
+  @colors = Array.new(n) { Array.new(n) { nil } }
+
+  puts optimal_multiplication(mixtures)
 end
