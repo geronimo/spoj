b54459af0e89627ea9488247c9f600cb5b2902e4
Geronimo <geronimodeabreu@gmail.com>
2012-11-07 00:50:16 -0430

    PALIN Accepted - C++

--- PALIN/palin.cpp
+++ PALIN/palin.cpp
@@ -36,9 +36,10 @@ int main(){
         if(test_case.size() == 1){
             if(test_case[0] == '9'){
                 cout << 11 << endl;
-                continue;
             }
-            cout << atoi(&test_case[0]) + 1 << endl;
+            else{
+                cout << atoi(&test_case[0]) + 1 << endl;
+            }
             continue;
         }
 
@@ -47,111 +48,79 @@ int main(){
         bool tailing_one = false;
         int size = test_case.size() - 1;
 
-        int last_index;
         if(test_case.size() % 2 == 1){
 
-            cout << "ENTRE" << endl;
-            FORI(i, 0, last_index){
-
-                if(test_case[i] == test_case[size - i]){
-                    cout << "IGUAL" << endl;
-
-                    if(i == size - i + 1){ // last element
-                        if(carry){
-                            if(test_case[i] == '9' && carry){
-                                for(int k = i; i >= 0; i--){
-                                    if(k != 0 && test_case[k] == '9'){
-                                        test_case[k] = test_case[size-k] = '0';
-                                    }else if(k == 0  && test_case[k] == '9'){
-                                        test_case[k] = '1';
-                                        test_case[size] = '0';
-                                        tailing_one = true;
-                                        break;
-                                    }else{
-                                        test_case[k] += 1;
-                                        test_case[size-k] += 1;
-                                        break;
-                                    }
-                                }
-                            }
-                            else if(test_case[i] != '9'){
-                                test_case[i] += 1;
-                                test_case[size - 1] += 1;
-                                changed = true;
-                            }
-                        }
-                        else{
-                            continue;
-                        }
-                    }
-                    else{
-                        continue;
-                    }
-
-                    if(i == size - i + 1 && carry){
-                        if(test_case[i] == '9'){
-                            test_case[i] = '0';
-                            for(int k = i - 1; i >= 0; i--){
-                                if(k != 0 && test_case[k] == '9'){
-                                    test_case[k] = test_case[size-k] = '0';
-                                }else if(k == 0  && test_case[k] == '9'){
-                                    test_case[k] = '1';
-                                    test_case[size] = '0';
-                                    tailing_one = true;
-                                    break;
-                                }else{
-                                    test_case[k] += 1;
-                                    test_case[size-k] += 1;
-                                    break;
-                                }
-                            }
-                        }
-                        else{
-                            changed = true;
-                            test_case[i] += 1;
-                            test_case[size - i] += 1;
-                        }
-                    }
-                    else if(i == size - i + 1 && !changed){
-                        if(test_case[i] == '9'){
-                            test_case[i] = '0';
-                            for(int k = i - 1; i >= 0; i--){
-                                if(k != 0 && test_case[k] == '9'){
-                                    test_case[k] = test_case[size-k] = '0';
-                                }else if(k == 0  && test_case[k] == '9'){
-                                    test_case[k] = '1';
-                                    test_case[size] = '0';
-                                    tailing_one = true;
-                                    break;
-                                }else{
-                                    test_case[k] += 1;
-                                    test_case[size-k] += 1;
-                                    break;
-                                }
-                            }
-                        }
-                        else {
-                            changed = true;
-                            test_case[i] += 1;
-                            test_case[size - i] += 1;
-                        }
-                    }
-                }
-                else if(test_case[i] > test_case[size - i]){
-                    cout << "MAYOR" << endl;
+            FORI(i, 0, test_case.size()/2){
+                if(test_case[i] > test_case[size - i]){
                     test_case[size - i] = test_case[i];
                     changed = true;
                     carry = false;
                 }
                 else if(test_case[i] < test_case[size - i]){
-                    cout << "MENOR" << endl;
                     test_case[size - i] = test_case[i];
                     carry = true;
                     changed = true;
                 }
             }
+
+            if(!changed || carry){
+                if(test_case[test_case.size()/2] != '9'){
+                    test_case[test_case.size()/2] += 1;
+                }
+                else{ 
+                    for(int k = test_case.size()/2; k >= 0; k--){
+                        if(k != 0 && test_case[k] == '9'){
+                            test_case[k] = test_case[size-k] = '0';
+                        }else if(k == 0  && test_case[k] == '9'){
+                            test_case[k] = '1';
+                            test_case[size] = '0';
+                            tailing_one = true;
+                            break;
+                        }else{
+                            test_case[k] += 1;
+                            test_case[size-k] += 1;
+                            break;
+                        }
+                    }
+                }
+            }
         }else{
 
+            FORI(i, 0, test_case.size()/2 - 1){
+                if(test_case[i] > test_case[size - i]){
+                    test_case[size - i] = test_case[i];
+                    changed = true;
+                    carry = false;
+                }
+                else if(test_case[i] < test_case[size - i]){
+                    test_case[size - i] = test_case[i];
+                    carry = true;
+                    changed = true;
+                }
+            }
+
+            if(!changed || carry){
+                if(test_case[test_case.size()/2 - 1] != '9'){
+                    test_case[test_case.size()/2] += 1;
+                    test_case[test_case.size()/2 - 1] += 1;
+                }
+                else{ 
+                    for(int k = test_case.size()/2 - 1; k >= 0; k--){
+                        if(k != 0 && test_case[k] == '9'){
+                            test_case[k] = test_case[size-k] = '0';
+                        }else if(k == 0  && test_case[k] == '9'){
+                            test_case[k] = '1';
+                            test_case[size] = '0';
+                            tailing_one = true;
+                            break;
+                        }else{
+                            test_case[k] += 1;
+                            test_case[size-k] += 1;
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
 
