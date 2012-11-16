f4d2c7aa164969babd0bcc9d1209d6b315788150
Geronimo <geronimodeabreu@gmail.com>
2012-11-16 00:03:41 -0430

    ARITH Accepted C++

--- ARITH/arith.cpp
+++ ARITH/arith.cpp
@@ -25,6 +25,31 @@
 
 using namespace std;
 
+string strings_mult(string first, char c){
+    if(c == '0') return "0";
+
+    string result = "";
+    int n = first.size();
+    int carry = 0;
+    for(int i=0; i<n; i++){
+        int mult = (c - '0') * (first[i] - '0');
+        mult += carry;
+
+        if(mult < 10){
+            result += (char) mult + '0';
+            carry = 0;
+        }
+        else{
+            result += (char) mult%10 + '0';
+            carry = mult / 10;
+        }
+    }
+    if(carry){
+        result += (char) carry + '0';
+    }
+    return result;
+}
+
 string strings_sum(string first, string last){
     int n;
     string result = "";
@@ -66,6 +91,13 @@ string strings_sum(string first, string last){
 }
 
 void print_sum_subs(string first, string last, string result, int n, char oper){
+    int zeros = 0;
+    for(int x=result.size()-1; x>=0; x--){
+        if(result[x] =='0') zeros++;
+        else break;
+    }
+    int dashes = max(last.size()+1, result.size()-zeros);
+
     if(n < result.size())
         n = result.size();
     else if(n <= last.size())
@@ -82,20 +114,16 @@ void print_sum_subs(string first, string last, string result, int n, char oper){
         else cout << last[i];
     }
     cout << endl;
+
     for(int i=n-1; i>=0; i--){
-        cout << '-';
+        if(i >= dashes) cout << " ";
+        else cout << '-';
     }
     cout << endl;
-    bool zero = true;
     for(int i=n-1; i>=0; i--){
-        if(i >= result.size()) cout << " ";
+        if(i >= result.size()-zeros) cout << " ";
         else{ 
-            if(zero && result[i] == '0'){
-                cout << " ";
-            }else{
-                cout << result[i];
-                zero = false;
-            }
+            cout << result[i];
         }
     }
     cout << endl;
@@ -171,10 +199,70 @@ int main(){
             if(i != 0 || sum != 0){
                 result += (char) sum + '0';
             }
+            if(i == 0 && sum == 0){
+                result += (char) sum + '0';
+            }
         }
         print_sum_subs(first, last, result, n, oper);
     }
     else{
+        int n = last.size();
+        vector< string > partials;
+        string total;
+
+        FOR(i, 0, n){
+            string tmp = strings_mult(first, last[i]);
+            partials.push_back(tmp);
+            if(i == 0) total = tmp;
+            else {
+                string z = "";
+                REP(j, i){ z += '0'; }
+                total = strings_sum(total, z + tmp);
+            }
+        }
+
+        int m = max(last.size()+1, partials[0].size());
+        int j = last.size()-1 + partials[partials.size() - 1].size();
+        int dashes = max((int)total.size(), j);
+        n = max(m, j);
+        n = max(n, (int)total.size());
+
+        for(int i=n-1; i>=0; i--){
+            if(i >= first.size()) cout << " ";
+            else cout << first[i];
+        }
+        cout << endl;
+        for(int i=n-1; i>=0; i--){
+            if(i > last.size()) cout << " ";
+            else if(i == last.size()) cout << oper;
+            else cout << last[i];
+        }
+        cout << endl;
+        for(int i=n-1; i>=0; i--){
+            if(i >= m) cout << " ";
+            else cout << '-';
+        }
+        cout << endl;
+
+        if(partials.size() > 1){
+            for(int x=0; x<partials.size(); x++){
+                for(int i=n-1-x; i>=0; i--){
+                    if(i >= partials[x].size()) cout << " ";
+                    else cout << partials[x][i];
+                }
+                cout << endl;
+            }
+            for(int i=n-1; i>=0; i--){
+                if(i >= dashes) cout << " ";
+                else cout << '-';
+            }
+            cout << endl;
+        }
+        for(int i=n-1; i>=0; i--){
+            if(i >= total.size()) cout << " ";
+            else cout << total[i];
+        }
+        cout << endl;
     }
     cout << endl;
   }
